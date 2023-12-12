371c6b15bddeb54dff5bd18d008a054182a12e15
Abidemi Omosanya <89324079+Bii-sama@users.noreply.github.com>
2023-12-12 13:20:14 +0100

    userModels

--- models/userModel.go
+++ models/userModel.go
@@ -1,17 +1,22 @@
 package model
 
 import (
+	"time"
+
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 
 type User struct{
-	ID        primitive.ObjectID
-	firstName string
-	lastName string
-	email string
-	password string
-	token string
-	refresh_token string
+	ID        primitive.ObjectID  `bson:"_id,omitempty"`
+	Firstname *string              `json:"first_name" validate:"required,min=3,max=100"`
+	Lastname *string               `json:"last_name"`
+	Email *string                  `json:"email"`
+	Password *string               `json:"password"`
+	Token *string                  `json:"token"`
+	Refresh_token *string          `json:"refresh_token"`
+	Created_at   time.Time
+	Updated_at   time.Time
+	User_ID string
 
 }
