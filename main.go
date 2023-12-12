ed27310be69657a8c50356f1965a1e922686c8ec
Abidemi Omosanya <89324079+Bii-sama@users.noreply.github.com>
2023-12-12 22:27:39 +0100

    router set-up

--- main.go
+++ main.go
@@ -1,6 +1,33 @@
 package main
 
+import(
+	routes "go-figure/routes"
+	"os"
+	"github.com/gin-gonic/gin"
+
+)
+
 
 func main()  {
-	
+	port := os.Getenv("PORT")
+
+	if port == ""{
+		port=  "8000"
+	}
+
+	router :=  gin.New()
+	router.Use(gin.Logger())
+
+	routes.Authroutes(router)
+	routes.UserRoutes(router)
+
+	router.GET("api-1", func(ctx *gin.Context) {
+		ctx.JSON(200,gin.H{"success": "Access granted for Api-1"})
+	})
+
+	router.GET("api-2", func(ctx *gin.Context) {
+		ctx.JSON(200,gin.H{"success": "Access granted for Api-2"})
+	})
+
+	router.Run(":" + port)
 }
--- models/userModel.go
+++ models/userModel.go
@@ -10,13 +10,13 @@ import (
 type User struct{
 	ID        primitive.ObjectID  `bson:"_id,omitempty"`
 	Firstname *string              `json:"first_name" validate:"required,min=3,max=100"`
-	Lastname *string               `json:"last_name"`
-	Email *string                  `json:"email"`
-	Password *string               `json:"password"`
+	Lastname *string               `json:"last_name" validate:"required,min=3,max=100"`
+	Email *string                  `json:"email" validate:"email, required"`
+	Password *string               `json:"password" validate:"required,min=6,max=50"`
 	Token *string                  `json:"token"`
 	Refresh_token *string          `json:"refresh_token"`
-	Created_at   time.Time
-	Updated_at   time.Time
-	User_ID string
+	Created_at   time.Time         `json:"created_at"`
+	Updated_at   time.Time         `json:"updated_at"`
+	User_ID string                 `json:"user_id"`
 
 }
