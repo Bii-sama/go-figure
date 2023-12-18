eb5274e2d8951c69d3f7bb736ae783352984f80b
Abidemi Omosanya <89324079+Bii-sama@users.noreply.github.com>
2023-12-18 23:00:02 +0100

    Signup and getAuser fuction done

--- controllers/userController.go
+++ controllers/userController.go
@@ -13,6 +13,7 @@ import (
 	"github.com/Bii-sama/go-figure.git/utils"
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"golang.org/x/crypto/bcrypt"
 )
@@ -28,8 +29,37 @@ func PasswordVerification()  {
 	
 }
 
-func SignUp()  {
-	
+func SignUp() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100 * time.Second)
+		var user models.User
+
+		if err := c.BindJSON(&user); err != nil{
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		validateErr := validate.Struct(user)
+
+		if validateErr != nil{
+			c.JSON(http.StatusBadRequest, gin.H{"error": validateErr.Error()})
+			return
+		}
+
+		count, err := userCollection.CountDocuments(ctx, bson.M{"email": user.Email})
+		defer cancel()
+
+		if err != nil{
+			log.Panicln(err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occured while checking for the email"})
+		}
+
+		if count > 0{
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "This email already exists"})
+		}
+
+		c.JSON(http.StatusOK, user)
+	}
 }
 
 func Login()  {
@@ -42,12 +72,24 @@ func GetUsers()  {
 
 func GetAUser () gin.HandlerFunc  {
 
-	return func(ctx *gin.Context) {
-		userId := ctx.Param("user_id")
+	return func(c *gin.Context) {
+		userId := c.Param("user_id")
 
-		if err := utils.CheckTypeEqualsUserId(ctx, userId); err != nil{
-			ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		if err := utils.CheckTypeEqualsUserId(c, userId); err != nil{
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 			return
 		}
+
+		var ctx, cancel = context.WithTimeout(context.Background(), 100 * time.Second)
+
+		var user models.User
+		err := userCollection.FindOne(ctx, bson.M{"user_id": userId}).Decode(&user)
+		defer cancel()
+
+		if err != nil{
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		}
+
+		c.JSON(http.StatusOK, user)
 	}
 }
