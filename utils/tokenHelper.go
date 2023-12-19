9b1817085537f2f76e8965bc8e9e52a013099bb3
Abidemi Omosanya <89324079+Bii-sama@users.noreply.github.com>
2023-12-19 22:17:28 +0100

    func genToken

--- utils/tokenHelper.go
+++ utils/tokenHelper.go
@@ -2,4 +2,32 @@ package utils
 
 import (
 	"github.com/gin-gonic/gin"
+	"fmt"
+	"log"
+	"context"
+	"os"
+	"time"
+	"github.com/Bii-sama/go-figure.git/database"
+	"github.com/dgrijalva/jwt-go"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
 )
+
+
+type SignUpInfo struct{
+	Email string
+	Firstname string
+	Lastname string
+	User_Type string
+	Uid string
+	jwt.StandardClaims
+}
+
+var userCollection *mongo.Collection = database.OpenCollection(database.Client, "user")
+
+var SECRET_KEY string = os.Getenv("JWT_SECRET")
+
+func GenerateAllTokens(email string, firstname string, lastname string, user_type string, uid string)  {
+	
+}
