5d6e415e65cc1e3020efcf81d81aa538fa3262bb
Abidemi Omosanya <89324079+Bii-sama@users.noreply.github.com>
2023-12-27 21:24:59 +0100

    getUsers func done

--- controllers/userController.go
+++ controllers/userController.go
@@ -5,7 +5,8 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"os/user"
+
+
 	"strconv"
 	"time"
 
@@ -157,16 +158,57 @@ func GetUsers() gin.HandlerFunc {
 
 	 var ctx, cancel = context.WithTimeout(context.Background(), 100 * time.Second)
 
-	var users models.User
+	 recordPerPage, err := strconv.Atoi(c.Query("recordPerPage"))
 
-	err := userCollection.Find(ctx, bson.M{"":}).Decode(&users)
+	 if err != nil || recordPerPage < 1{
+		recordPerPage = 10
+	 }
+	 page, err1  := strconv.Atoi(c.Query("page"))
+
+	 if err1 != nil || page < 1{
+		page = 1
+	 }
+
+	 startIndex := (page - 1)  * recordPerPage
+
+	 startIndex, err := strconv.Atoi(c.Query("startIndex"))
+
+	 matchStage := bson.D{{Key:"$match", Value: bson.D{{}}}}
+	 groupStage := bson.D{
+		{
+			Key: "$group",
+			Value: bson.D{
+				{Key: "_id", Value: "null"},
+				{Key: "total_count", Value: bson.D{{Key: "$sum", Value: 1}}},
+				{Key: "data", Value: bson.D{{Key: "$push", Value: "$$ROOT"}}},
+			},
+		},
+	}
+	projectStage := bson.D{
+		{Key: "$project", Value: bson.D{
+			{Key: "_id", Value: 0},
+			{Key: "total_count", Value: 1},
+			{Key: "user_items", Value: bson.D{
+				{Key: "$slice", Value: []interface{}{"$data", startIndex ,recordPerPage}},
+			}},
+		}},
+	}
+
+	result, err:= userCollection.Aggregate(ctx, mongo.Pipeline{
+		matchStage, groupStage, projectStage,
+	})
 	defer cancel()
 
 	if err != nil{
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		c.JSON(http.StatusInternalServerError, gin.H{"error":"error occured while listing user items"})
 	}
 
-	c.JSON(http.StatusOK, users)
+	var users []bson.M
+
+	if err = result.All(ctx, &users); err != nil{
+		log.Fatalln(err)
+	}
+	c.JSON(http.StatusOK, users[0])
 	}
 
 	
