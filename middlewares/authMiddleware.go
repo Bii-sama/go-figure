bdc1267ea849db4867abf79da56426dbc4cabb2b
Abidemi Omosanya <89324079+Bii-sama@users.noreply.github.com>
2023-12-28 22:43:03 +0100

    working on authentication

--- middlewares/authMiddleware.go
+++ middlewares/authMiddleware.go
@@ -1,1 +1,21 @@
 package middlewares
+
+import(
+	"fmt"
+	"net/http"
+	"github.com/Bii-sama/go-figure.git/utils"
+	"github.com/gin-gonic/gin"
+)
+
+
+func Auth()gin.HandlerFunc {
+	return func(c *gin.Context) {
+		clientToken := c.Request.Header.Get("token")
+
+		if clientToken == ""{
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "No Authorization header provided"})
+			c.Abort()
+			return
+		}
+	}
+}
