c29eea97b6a68f43b498b1e3ae3cca39c2f56851
Abidemi Omosanya <89324079+Bii-sama@users.noreply.github.com>
2024-01-02 11:18:34 +0100

    working on bill controllers

--- controllers/billController.go
+++ controllers/billController.go
@@ -1,8 +1,29 @@
 package controllers
 
 import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
 
+
+	"strconv"
+	"time"
+
+	"github.com/Bii-sama/go-figure.git/database"
+	"github.com/Bii-sama/go-figure.git/models"
+	"github.com/Bii-sama/go-figure.git/utils"
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"golang.org/x/crypto/bcrypt"
 )
+
+
+var billCollection *mongo.Collection = database.NewCollection(database.Client, "bills")
+var validateBill = validator.New()
 
 func GetAllBills()  {
 	
@@ -12,8 +33,22 @@ func GetABill()  {
 	
 }
 
-func CreateBill()  {
-	
+func CreateBill() gin.HandlerFunc  {
+	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 50 * time.Second)
+		defer cancel()
+		var bill models.Bill
+
+		if err := c.BindJSON(&bill); err != nil{
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		}
+
+		isBillValid := validateBill.Struct(bill)
+
+		if isBillValid != nil{
+			c.JSON(http.StatusInternalServerError, gin.H{"error": isBillValid.Error()})
+		}
+	}
 }
 
 func UpdateBill()  {
--- models/billModel.go
+++ models/billModel.go
@@ -10,11 +10,13 @@ import (
 
 type Bill struct{
 	ID        primitive.ObjectID  `bson:"_id,omitempty"`
+	Created_by *string             `json:"created_by" validate:"required,min=3,max=100"`
 	Firstname *string              `json:"first_name" validate:"required,min=3,max=100"`
 	Items map[string]float64       `json:"items" validate:"required"`
 	Email *string                  `json:"email" validate:"email"`
 	Created_at   time.Time         `json:"created_at"`
 	Updated_at   time.Time         `json:"updated_at"`
 	Bill_ID string                 `json:"bill_id"`
+
 
 }
